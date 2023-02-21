1d6e7c56dec5b3121168ad87012fefbdd61a32ae
ningxiao <dsgrichard@hotmail.com>
2023-02-20 19:55:49 -0800

    update

--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
@@ -9,16 +9,18 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
-    return false if board.tied?
-    return true if board.over? && board.winner != evaluator
-    return false if board.over? && (board.winner == evaluator || board.winner.nil?)
-    if self.next_mover_mark != evaluator
-      self.children.each do |child_node|
-        child_node.losing_node?(evaluator)
-      end
-    else
-      self.children.any? {|child_node| child_node.losing_node?(evaluator)}
+    opponent = (evaluator == :x ? :o : :x)
+    
+    if board.over?
+      return board.won? && board.winner != evaluator
     end
+    if self.next_mover_mark == evaluator
+      return self.children.all? {|child_node| child_node.losing_node?(evaluator)}
+      
+    else
+      return self.children.any? {|child_node| child_node.losing_node?(evaluator)}
+    end
+    
   end
 
   def winning_node?(evaluator)
@@ -27,14 +29,35 @@ class TicTacToeNode
 
   # This method generates an array of all moves that can be made after
   # the current move.
+  # def children
+  #   child_mark = (next_mover_mark == :x ? :o : :x)
+  #   nodes = []
+  #   board.open_positions.each do |child_pos|
+  #     new_board = board.dup
+  #     new_board[child_pos] = child_mark
+  #     nodes << TicTacToeNode.new(new_board, child_mark, child_pos)
+  #   end
+  #   nodes
+  # end
+  
   def children
-    child_mark = (next_mover_mark == :x ? :o : :x)
-    nodes = []
-    board.dup.open_positions.each do |child_pos|
-      new_board = board.dup
-      new_board[child_pos] = child_mark
-      nodes << TicTacToeNode.new(new_board, child_mark, child_pos)
+    children = []
+
+    (0..2).each do |row_idx|
+      (0..2).each do |col_idx|
+        pos = [row_idx, col_idx]
+
+        # Can't move here if it's not free
+        next unless board.empty?(pos)
+
+        new_board = board.dup
+        new_board[pos] = self.next_mover_mark
+        next_mover_mark = (self.next_mover_mark == :x ? :o : :x)
+
+        children << TicTacToeNode.new(new_board, next_mover_mark, pos)
+      end
     end
-    nodes
+
+    children
   end
 end
